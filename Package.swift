71b7ced8f6c426f4454def6e298c23caa1aa1151
Mihael Isaev <isaev.mihael@gmail.com>
2020-02-29 14:18:27 +0400

    Package: bump macOS platform to 10_15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "VaporCron",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "VaporCron", targets: ["VaporCron"]),
