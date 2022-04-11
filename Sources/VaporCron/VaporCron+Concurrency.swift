f6abaef15a8948c4bfaf2f4808432315115da2fa
iWe <16604027+iWECon@users.noreply.github.com>
2022-04-11 23:37:55 +0800

    Support `Concurrency` by @iWECon
    Co-authored-by: iWw <i.1214@yahoo.com>

---
+++ Sources/VaporCron/VaporCron+Concurrency.swift
@@ -0,0 +1,42 @@
+#if compiler(>=5.5) && canImport(_Concurrency)
+import Vapor
+
+// MARK: VaporCronSchedulable
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+public protocol AsyncVaporCronSchedulable: VaporCronSchedulable {
+    static func task(on application: Application) async throws -> T
+}
+
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+extension AsyncVaporCronSchedulable {
+    
+    public static func task(on application: Application) -> EventLoopFuture<T> {
+        let promise = application.eventLoopGroup.next().makePromise(of: T.self)
+        promise.completeWithTask {
+            try await task(on: application)
+        }
+        return promise.futureResult
+    }
+    
+}
+
+// MARK: VaporCronInstanceSchedulable
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+public protocol AsyncVaporCronInstanceSchedulable: VaporCronInstanceSchedulable {
+    func task(on application: Application) async throws -> T
+}
+
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+extension AsyncVaporCronInstanceSchedulable {
+    
+    public func task(on application: Application) -> EventLoopFuture<T> {
+        let promise = application.eventLoopGroup.next().makePromise(of: T.self)
+        promise.completeWithTask {
+            try await task(on: application)
+        }
+        return promise.futureResult
+    }
+    
+}
+#endif
+
