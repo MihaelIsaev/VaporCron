855f13acc014d161317faf51075bb062168feb3d
iWe <iwecon@aol.com>
2022-04-16 02:29:20 +0800

    support error handle

--- Sources/VaporCron/VaporCron+Concurrency.swift
+++ Sources/VaporCron/VaporCron+Concurrency.swift
@@ -5,38 +5,49 @@ import Vapor
 @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 public protocol AsyncVaporCronSchedulable: VaporCronSchedulable {
     static func task(on application: Application) async throws -> T
+    static func error(_ error: Error, on application: Application)
 }
 
 @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 extension AsyncVaporCronSchedulable {
-    
     public static func task(on application: Application) -> EventLoopFuture<T> {
         let promise = application.eventLoopGroup.next().makePromise(of: T.self)
         promise.completeWithTask {
             try await task(on: application)
         }
-        return promise.futureResult
+        
+        let result = promise.futureResult
+        result.whenFailure { error in
+            self.error(error, on: application)
+        }
+        return result
     }
-    
+    public static func error(_ error: Error, on application: Application) {
+        application.logger.error("[\(Self.self)] \(error)")
+    }
 }
 
 // MARK: VaporCronInstanceSchedulable
 @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 public protocol AsyncVaporCronInstanceSchedulable: VaporCronInstanceSchedulable {
     func task() async throws -> T
+    func error(_ error: Error)
 }
 
 @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 extension AsyncVaporCronInstanceSchedulable {
-    
     public func task() -> EventLoopFuture<T> {
         let promise = application.eventLoopGroup.next().makePromise(of: T.self)
         promise.completeWithTask {
             try await task()
         }
-        return promise.futureResult
+        let result = promise.futureResult
+        result.whenFailure(self.error(_:))
+        return result
     }
-    
+    public func error(_ error: Error) {
+        application.logger.error("[\(Self.self)] \(error)")
+    }
 }
 #endif
 
